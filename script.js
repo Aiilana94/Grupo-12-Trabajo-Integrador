d9d41a70bcaa35a3b99aaad6b0f637e22acf23da
Aiilana94 <analia-jo@hotmail.com>
2023-04-08 17:57:59 -0300

    Primer Cambio

---
+++ script.js
@@ -0,0 +1,13 @@
+      function enviar() {
+        if (document.getElementById("nombre").checkValidity() && document.getElementById("email").checkValidity() && document.getElementById("telefono").checkValidity() 
+        && document.getElementById("assunto").checkValidity() && document.getElementById("mensaje").checkValidity()) {
+          alert("Mensaje enviado");
+          document.getElementById("nombre").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("telefono").value = "";
+          document.getElementById("assunto").value = "";
+          document.getElementById("mensaje").value = "";
+        } else {
+          alert("Por favor completa los campos obligatorios");
+        }
+      }
